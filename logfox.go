fd27d754d34d4c286a9dabe605193bc6f54a10f8
五环小胖子 <zer0131@vip.qq.com>
2018-03-08 20:09:02 +0800

    新增

--- logfox.go
+++ logfox.go
@@ -1,7 +1,24 @@
 package logfox
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func Debug(v ...interface{})  {
+const (
+	DEFAULT_FILEWRITER_MAX_BACKUP_DAY          = 7
+	DEFAULT_FILEWRITER_SPLIT_DURATION          = time.Hour
+	DEFAULT_FILEWRITER_FILE_SUFFIX_TIME_STRING = "2006010215"
+	DEFAULT_FILEWRITER_MSG_SUFFIX_TIME_STRING  = "06-01-02 15:04:05.999"
+)
+
+var loggerObj *logger = nil
+var LogLevel string = "DEBUG"
+
+func Init(path string, app string) error {
+	return nil
+}
+
+func Debug(v ...interface{}) {
 	fmt.Println(v...)
 }
